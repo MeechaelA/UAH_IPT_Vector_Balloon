38e46b5d353d4958111e3fe230a37fb0f57b6489
MeechaelA <44931960+MeechaelA@users.noreply.github.com>
2019-11-01 11:16:06 -0500

    Update Venus_Atmos_Model.m
    Fixed model functions.

--- Venus_Atmos_Model.m
+++ Venus_Atmos_Model.m
@@ -1,12 +1,12 @@
-%% Figus
+%% Figures
 clear,clc
 
 %Givens using "Structure of the Venusian Atmosphere" Paper
-Table2 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_Less_Than_35', 'Range','A3:I53');
-Table3 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_35_55', 'Range','A3:I53');
-Table4 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_50_70', 'Range','A3:I53');
-Table5 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_70_80', 'Range','A3:I53');
-Table6 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_85', 'Range','A3:I53');
+Table2 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_Less_Than_35', 'Range','M3:U54');
+Table3 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_35_55', 'Range','M3:U54');
+Table4 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_50_70', 'Range','M3:U54');
+Table5 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_BW_70_80', 'Range','M3:U54');
+Table6 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Phi_85', 'Range','M3:U54');
 Table7 = readtable('Venus_Atmos.xlsx', 'Sheet', 'Density', 'Range','A2:E27');
 
 n = 39
@@ -26,148 +26,155 @@ N_len = length(Table7.NumberDensity) - l
 M_len = length(Table7.MolecularMass) - l
 Visc_len = length(Table7.Viscosity) - l
 
-
 %% Table Two Functions - Returns Heqight
-    
-P_Bar_Function_Eq_Two = polyfit(Table2.P_Bar,Table2.Var1,P_len);
-P_Bar_Function_Two = @(x) polyval(P_Bar_Function_Eq_Two,x);
 
-P_Bar_1_Function_Eq_Two = polyfit(Table2.P_Bar_1,Table2.Var1,P_len);
-P_Bar_1_Function_Two = @(x) polyval(P_Bar_1_Function_Eq_Two,x);
+P_Bar_Function_Eq_Two = fit(Table2.P_Bar,Table2.Var1,'cubicinterp');
+P_Bar_Function_Two = @(x) feval(P_Bar_Function_Eq_Two,x);
 
-P_Bar_2_Function_Eq_Two = polyfit(Table2.P_Bar_2,Table2.Var1,P_len);
-P_Bar_2_Function_Two = @(x) polyval(P_Bar_2_Function_Eq_Two,x);
+P_Bar_1_Function_Eq_Two = fit(Table2.P_Bar_1,Table2.Var1,'cubicinterp');
+P_Bar_1_Function_Two = @(x) feval(P_Bar_1_Function_Eq_Two,x);
 
-P_Bar_3_Function_Eq_Two = polyfit(Table2.P_Bar_3,Table3.Var1,P_len);
-P_Bar_3_Function_Two = @(x) polyval(P_Bar_3_Function_Eq_Two,x);
+P_Bar_2_Function_Eq_Two = fit(Table2.P_Bar_2,Table2.Var1,'cubicinterp');
+P_Bar_2_Function_Two = @(x) feval(P_Bar_2_Function_Eq_Two,x);
 
-T_K_Function_Eq_Two = polyfit(Table2.T_K,Table2.Var1,T_len);
-T_K_Function_Two = @(x) polyval(T_K_Function_Eq_Two,x);
+P_Bar_3_Function_Eq_Two = fit(Table2.P_Bar_3,Table3.Var1,'cubicinterp');
+P_Bar_3_Function_Two = @(x) feval(P_Bar_3_Function_Eq_Two,x);
 
-T_K_1_Function_Eq_Two = polyfit(Table2.T_K_1,Table2.Var1,T_len);
-T_K_1_Function_Two = @(x) polyval(T_K_1_Function_Eq_Two,x);
+options_temp = fitoptions('Method','linearinterp')
 
-T_K_2_Function_Eq_Two = polyfit(Table2.T_K_2,Table2.Var1,T_len);
-T_K_2_Function_Two = @(x) polyval(T_K_2_Function_Eq_Two,x);
+T_K_Function_Eq_Two = fit(Table2.T_K,Table2.Var1,'linear',options_temp);
+T_K_Function_Two = @(x) feval(T_K_Function_Eq_Two,x);
 
-T_K_3_Function_Eq_Two = polyfit(Table2.T_K_3,Table2.Var1,T_len);
-T_K_3_Function_Two = @(x) polyval(T_K_3_Function_Eq_Two,x);
+T_K_1_Function_Eq_Two = fit(Table2.T_K_1,Table2.Var1,'linear',options_temp);
+T_K_1_Function_Two = @(x) feval(T_K_1_Function_Eq_Two,x);
+
+T_K_2_Function_Eq_Two = fit(Table2.T_K_2,Table2.Var1,'linear',options_temp);
+T_K_2_Function_Two = @(x) feval(T_K_2_Function_Eq_Two,x);
+
+T_K_3_Function_Eq_Two = fit(Table2.T_K_3,Table2.Var1,'linear',options_temp);
+T_K_3_Function_Two = @(x) feval(T_K_3_Function_Eq_Two,x);
 
 %% Table Three Functions - Returns Height
-P_Bar_Function_Eq_Three = polyfit(Table3.P_Bar,Table3.Var1,P_len);
-P_Bar_Function_Three = @(x) polyval(P_Bar_Function_Eq_Three,x);
+P_Bar_Function_Eq_Three = fit(Table3.P_Bar,Table3.Var1,'cubicinterp');
+P_Bar_Function_Three = @(x) feval(P_Bar_Function_Eq_Three,x);
 
-P_Bar_1_Function_Eq_Three = polyfit(Table3.P_Bar_1,Table3.Var1,P_len);
-P_Bar_1_Function_Three = @(x) polyval(P_Bar_1_Function_Eq_Three,x);
+P_Bar_1_Function_Eq_Three = fit(Table3.P_Bar_1,Table3.Var1,'cubicinterp');
+P_Bar_1_Function_Three = @(x) feval(P_Bar_1_Function_Eq_Three,x);
 
-P_Bar_2_Function_Eq_Three = polyfit(Table3.P_Bar_2,Table3.Var1,P_len);
-P_Bar_2_Function_Three = @(x) polyval(P_Bar_2_Function_Eq_Three,x);
+P_Bar_2_Function_Eq_Three = fit(Table3.P_Bar_2,Table3.Var1,'cubicinterp');
+P_Bar_2_Function_Three = @(x) feval(P_Bar_2_Function_Eq_Three,x);
 
-P_Bar_3_Function_Eq_Three = polyfit(Table3.P_Bar_3,Table3.Var1,P_len);
-P_Bar_3_Function_Three = @(x) polyval(P_Bar_3_Function_Eq_Three,x);
+P_Bar_3_Function_Eq_Three = fit(Table3.P_Bar_3,Table3.Var1,'cubicinterp');
+P_Bar_3_Function_Three = @(x) feval(P_Bar_3_Function_Eq_Three,x);
 
-T_K_Function_Eq_Three = polyfit(Table3.T_K,Table3.Var1,T_len);
-T_K_Function_Three = @(x) polyval(T_K_Function_Eq_Three,x);
+T_K_Function_Eq_Three = fit(Table3.T_K,Table3.Var1,'linear',options_temp);
+T_K_Function_Three = @(x) feval(T_K_Function_Eq_Three,x);
 
-T_K_1_Function_Eq_Three = polyfit(Table3.T_K_1,Table3.Var1,T_len);
-T_K_1_Function_Three = @(x) polyval(T_K_1_Function_Eq_Three,x);
+T_K_1_Function_Eq_Three = fit(Table3.T_K_1,Table3.Var1,'linear',options_temp);
+T_K_1_Function_Three = @(x) feval(T_K_1_Function_Eq_Three,x);
 
-T_K_2_Function_Eq_Three = polyfit(Table3.T_K_2,Table3.Var1,T_len);
-T_K_2_Function_Three = @(x) polyval(T_K_2_Function_Eq_Three,x);
+T_K_2_Function_Eq_Three = fit(Table3.T_K_2,Table3.Var1,'linear',options_temp);
+T_K_2_Function_Three = @(x) feval(T_K_2_Function_Eq_Three,x);
 
-T_K_3_Function_Eq_Three = polyfit(Table3.T_K_3,Table3.Var1,T_len);
-T_K_3_Function_Three = @(x) polyval(T_K_3_Function_Eq_Three,x);
+T_K_3_Function_Eq_Three = fit(Table3.T_K_3,Table3.Var1,'linear',options_temp);
+T_K_3_Function_Three = @(x) feval(T_K_3_Function_Eq_Three,x);
+
 %% Table Four Functions - Returns Height
-P_Bar_Function_Eq_Four = polyfit(Table4.P_Bar,Table4.Var1,P_len);
-P_Bar_Function_Four = @(x) polyval(P_Bar_Function_Eq_Four,x);
+P_Bar_Function_Eq_Four = fit(Table4.P_Bar,Table4.Var1,'cubicinterp');
+P_Bar_Function_Four = @(x) feval(P_Bar_Function_Eq_Four,x);
 
-P_Bar_1_Function_Eq_Four = polyfit(Table4.P_Bar_1,Table4.Var1,P_len);
-P_Bar_1_Function_Four = @(x) polyval(P_Bar_1_Function_Eq_Four,x);
+P_Bar_1_Function_Eq_Four = fit(Table4.P_Bar_1,Table4.Var1,'cubicinterp');
+P_Bar_1_Function_Four = @(x) feval(P_Bar_1_Function_Eq_Four,x);
 
-P_Bar_2_Function_Eq_Four = polyfit(Table4.P_Bar_2,Table4.Var1,P_len);
-P_Bar_2_Function_Four = @(x) polyval(P_Bar_2_Function_Eq_Four,x);
+P_Bar_2_Function_Eq_Four = fit(Table4.P_Bar_2,Table4.Var1,'cubicinterp');
+P_Bar_2_Function_Four = @(x) feval(P_Bar_2_Function_Eq_Four,x);
 
-P_Bar_3_Function_Eq_Four = polyfit(Table4.P_Bar_3,Table4.Var1,P_len);
-P_Bar_3_Function_Four = @(x) polyval(P_Bar_3_Function_Eq_Four,x);
+P_Bar_3_Function_Eq_Four = fit(Table4.P_Bar_3,Table4.Var1,'cubicinterp');
+P_Bar_3_Function_Four = @(x) feval(P_Bar_3_Function_Eq_Four,x);
 
-T_K_Function_Eq_Four = polyfit(Table4.T_K,Table4.Var1,T_len);
-T_K_Function_Four = @(x) polyval(T_K_Function_Eq_Four,x);
+options_temp2 = fitoptions('Method','linearinterp', 'Normalize','on')
 
-T_K_1_Function_Eq_Four = polyfit(Table4.T_K_1,Table4.Var1,T_len);
-T_K_1_Function_Four = @(x) polyval(T_K_1_Function_Eq_Four,x);
+T_K_Function_Eq_Four = fit(Table4.T_K,Table4.Var1,'linear',options_temp2);
+T_K_Function_Four = @(x) feval(T_K_Function_Eq_Four,x);
 
-T_K_2_Function_Eq_Four = polyfit(Table4.T_K_2,Table4.Var1,T_len);
-T_K_2_Function_Four = @(x) polyval(T_K_2_Function_Eq_Four,x);
+T_K_1_Function_Eq_Four = fit(Table4.T_K_1,Table4.Var1,'linear',options_temp2);
+T_K_1_Function_Four = @(x) feval(T_K_1_Function_Eq_Four,x);
 
-T_K_3_Function_Eq_Four = polyfit(Table4.T_K_3,Table4.Var1,T_len);
-T_K_3_Function_Four = @(x) polyval(T_K_3_Function_Eq_Four,x);
+T_K_2_Function_Eq_Four = fit(Table4.T_K_2,Table4.Var1,'linear',options_temp2);
+T_K_2_Function_Four = @(x) feval(T_K_2_Function_Eq_Four,x);
+
+T_K_3_Function_Eq_Four = fit(Table4.T_K_3,Table4.Var1,'linear',options_temp);
+T_K_3_Function_Four = @(x) feval(T_K_3_Function_Eq_Four,x);
+
 %% Table Five Functions - Returns Height
-P_Bar_Function_Eq_Five = polyfit(Table5.P_Bar,Table5.Var1,P_len);
-P_Bar_Function_Five = @(x) polyval(P_Bar_Function_Eq_Five,x);
+P_Bar_Function_Eq_Five = fit(Table5.P_Bar,Table5.Var1,'cubicinterp');
+P_Bar_Function_Five = @(x) feval(P_Bar_Function_Eq_Five,x);
 
-P_Bar_1_Function_Eq_Five = polyfit(Table5.P_Bar_1,Table5.Var1,P_len);
-P_Bar_1_Function_Five = @(x) polyval(P_Bar_1_Function_Eq_Five,x);
+P_Bar_1_Function_Eq_Five = fit(Table5.P_Bar_1,Table5.Var1,'cubicinterp');
+P_Bar_1_Function_Five = @(x) feval(P_Bar_1_Function_Eq_Five,x);
 
-P_Bar_2_Function_Eq_Five = polyfit(Table5.P_Bar_2,Table5.Var1,P_len);
-P_Bar_2_Function_Five = @(x) polyval(P_Bar_2_Function_Eq_Five,x);
+P_Bar_2_Function_Eq_Five = fit(Table5.P_Bar_2,Table5.Var1,'cubicinterp');
+P_Bar_2_Function_Five = @(x) feval(P_Bar_2_Function_Eq_Five,x);
 
-P_Bar_3_Function_Eq_Five = polyfit(Table5.P_Bar_3,Table5.Var1,P_len);
-P_Bar_3_Function_Five = @(x) polyval(P_Bar_3_Function_Eq_Five,x);
+P_Bar_3_Function_Eq_Five = fit(Table5.P_Bar_3,Table5.Var1,'cubicinterp');
+P_Bar_3_Function_Five = @(x) feval(P_Bar_3_Function_Eq_Five,x);
 
-T_K_Function_Eq_Five = polyfit(Table5.T_K,Table5.Var1,T_len);
-T_K_Function_Five = @(x) polyval(T_K_Function_Eq_Five,x);
+T_K_Function_Eq_Five = fit(Table5.T_K,Table5.Var1,'linear',options_temp2);
+T_K_Function_Five = @(x) feval(T_K_Function_Eq_Five,x);
 
-T_K_1_Function_Eq_Five = polyfit(Table5.T_K_1,Table5.Var1,T_len);
-T_K_1_Function_Five = @(x) polyval(T_K_1_Function_Eq_Five,x);
+T_K_1_Function_Eq_Five = fit(Table5.T_K_1,Table5.Var1,'linear',options_temp2);
+T_K_1_Function_Five = @(x) feval(T_K_1_Function_Eq_Five,x);
 
-T_K_2_Function_Eq_Five = polyfit(Table5.T_K_2,Table5.Var1,T_len);
-T_K_2_Function_Five = @(x) polyval(T_K_2_Function_Eq_Five,x);
+T_K_2_Function_Eq_Five = fit(Table5.T_K_2,Table5.Var1,'linear',options_temp2);
+T_K_2_Function_Five = @(x) feval(T_K_2_Function_Eq_Five,x);
 
-T_K_3_Function_Eq_Five = polyfit(Table5.T_K_3,Table5.Var1,T_len);
-T_K_3_Function_Five = @(x) polyval(T_K_3_Function_Eq_Five,x);
+T_K_3_Function_Eq_Five = fit(Table5.T_K_3,Table5.Var1,'linear',options_temp2);
+T_K_3_Function_Five = @(x) feval(T_K_3_Function_Eq_Five,x);
+
 %% Table 6 Functions - Returns Height
-P_Bar_Function_Eq_Six = polyfit(Table6.P_Bar,Table6.Var1,P_len);
-P_Bar_Function_Six = @(x) polyval(P_Bar_Function_Eq_Six,x);
+P_Bar_Function_Eq_Six = fit(Table6.P_Bar,Table6.Var1,'cubicinterp');
+P_Bar_Function_Six = @(x) feval(P_Bar_Function_Eq_Six,x);
 
-P_Bar_1_Function_Eq_Six = polyfit(Table6.P_Bar_1,Table6.Var1,P_len);
-P_Bar_1_Function_Six = @(x) polyval(P_Bar_1_Function_Eq_Six,x);
+P_Bar_1_Function_Eq_Six = fit(Table6.P_Bar_1,Table6.Var1,'cubicinterp');
+P_Bar_1_Function_Six = @(x) feval(P_Bar_1_Function_Eq_Six,x);
 
-P_Bar_2_Function_Eq_Six = polyfit(Table6.P_Bar_2,Table6.Var1,P_len);
-P_Bar_2_Function_Six = @(x) polyval(P_Bar_2_Function_Eq_Six,x);
+P_Bar_2_Function_Eq_Six = fit(Table6.P_Bar_2,Table6.Var1,'cubicinterp');
+P_Bar_2_Function_Six = @(x) feval(P_Bar_2_Function_Eq_Six,x);
 
-P_Bar_3_Function_Eq_Six = polyfit(Table6.P_Bar_3,Table6.Var1,P_len);
-P_Bar_3_Function_Six = @(x) polyval(P_Bar_3_Function_Eq_Six,x);
+P_Bar_3_Function_Eq_Six = fit(Table6.P_Bar_3,Table6.Var1,'cubicinterp');
+P_Bar_3_Function_Six = @(x) feval(P_Bar_3_Function_Eq_Six,x);
 
-T_K_Function_Eq_Six = polyfit(Table6.T_K,Table6.Var1,T_len);
-T_K_Function_Six = @(x) polyval(T_K_Function_Eq_Six,x);
+T_K_Function_Eq_Six = fit(Table6.T_K,Table6.Var1,'linear',options_temp);
+T_K_Function_Six = @(x) feval(T_K_Function_Eq_Six,x);
 
-T_K_1_Function_Eq_Six = polyfit(Table6.T_K_1,Table6.Var1,T_len);
-T_K_1_Function_Six = @(x) polyval(T_K_1_Function_Eq_Six,x);
+T_K_1_Function_Eq_Six = fit(Table6.T_K_1,Table6.Var1,'linear',options_temp);
+T_K_1_Function_Six = @(x) feval(T_K_1_Function_Eq_Six,x);
 
-T_K_2_Function_Eq_Six = polyfit(Table6.T_K_2,Table6.Var1,T_len);
-T_K_2_Function_Six = @(x) polyval(T_K_2_Function_Eq_Six,x);
+T_K_2_Function_Eq_Six = fit(Table6.T_K_2,Table6.Var1,'linear',options_temp);
+T_K_2_Function_Six = @(x) feval(T_K_2_Function_Eq_Six,x);
 
-T_K_3_Function_Eq_Six = polyfit(Table6.T_K_3,Table2.Var1,T_len);
-T_K_3_Function_Six = @(x) polyval(T_K_3_Function_Eq_Six,x);
+T_K_3_Function_Eq_Six = fit(Table6.T_K_3,Table2.Var1,'linear',options_temp);
+T_K_3_Function_Six = @(x) feval(T_K_3_Function_Eq_Six,x);
+
 %% Table 7 - Functions - Returns Height
 
-Density_Function_Eq = polyfit(Table7.Density.*1000,Table7.Altitude,rho_len);
-Density_Function = @(x) polyval(Density_Function_Eq,x);
+Density_Function_Eq = fit(Table7.Density.*1000,Table7.Altitude,'cubicinterp');
+Density_Function = @(x) feval(Density_Function_Eq,x);
 Density = Table7.Density*1000;
 
 Molecular_Mass_Function_Eq = polyfit(Table7.MolecularMass,Table7.Altitude,1);
-Molecular_Mass_Function = @(x) Molecular_Mass_Function_Eq(1).*x+Molecular_Mass_Function_Eq(2);
+Molecular_Mass_Function = @(x) polyval(Molecular_Mass_Function_Eq,x);
 
-Number_Density_Function_Eq = polyfit(Table7.NumberDensity,Table7.Altitude,2);
-Number_Density_Function = @(x) Number_Density_Function_Eq(1).*x+Number_Density_Function_Eq(2);
+Number_Density_Function_Eq = fit(Table7.NumberDensity,Table7.Altitude,'cubicinterp');
+Number_Density_Function = @(x) feval(Number_Density_Function_Eq,x);
 
+Viscosity_Function_Eq = fit(Table7.Viscosity,Table7.Altitude,'cubicinterp');
+Viscosity_Function = @(x) feval(Viscosity_Function_Eq,x);
 
-Viscosity_Function_Eq = polyfit(Table7.Viscosity,Table7.Altitude,6);
-Viscosity_Function = @(x) Viscosity_Function_Eq(1).*x.^6+Viscosity_Function_Eq(2).*x.^5+Viscosity_Function_Eq(3).*x.^4+Viscosity_Function_Eq(4).*x.^3+Viscosity_Function_Eq(5).*x.^2+Viscosity_Function_Eq(6).*x+Viscosity_Function_Eq(7);
 
 %% Figures
 figure(1)
-%sgtitle('\phi < 35^{\circ}')
+sgtitle('\phi < 35^{\circ}')
 subplot(2,4,1)
 plot(Table2.P_Bar,Table2.Var1,'bo',Table2.P_Bar,P_Bar_Function_Two(Table2.P_Bar),'r-') 
 
@@ -212,7 +219,7 @@ ylabel('Height [km]')
 
 
 figure(2)
-%sgtitle('\phi = 35^{\circ} - 55^{\circ}')
+sgtitle('\phi = 35^{\circ} - 55^{\circ}')
 subplot(2,4,1)
 plot(Table3.P_Bar,Table3.Var1,'bo',Table3.P_Bar, P_Bar_Function_Three(Table3.P_Bar),'r-');
 
@@ -243,7 +250,7 @@ title('L_s = 200^{\circ}-270^{\circ}')
 xlabel('Pressure [bar]')
 ylabel('Height [km]')
 subplot(2,4,7)
-plot(Table3.T_K_2,Table3.Var1,'ro',Table3.T_K_2, T_K_Function_Three(Table3.T_K_2),'b-');
+plot(Table3.T_K_2,Table3.Var1,'ro',Table3.T_K_2, T_K_2_Function_Three(Table3.T_K_2),'b-');
 xlabel('Temperature [k]')
 ylabel('Height [km]')
 
@@ -259,7 +266,7 @@ ylabel('Height [km]')
 
 
 figure(3)
-%sgtitle('\phi = 50^{\circ} - 70^{\circ}')
+sgtitle('\phi = 50^{\circ} - 70^{\circ}')
 subplot(2,4,1)
 plot(Table4.P_Bar,Table4.Var1,'bo',Table4.P_Bar,P_Bar_Function_Four(Table4.P_Bar),'r-') 
 title('L_s = 20^{\circ}-90^{\circ}')
@@ -305,7 +312,7 @@ ylabel('Height [km]')
 
 
 figure(4)
-%sgtitle('\phi = 70^{\circ} - 80^{\circ}')
+sgtitle('\phi = 70^{\circ} - 80^{\circ}')
 subplot(2,4,1)
 plot(Table5.P_Bar,Table5.Var1,'bo',Table5.P_Bar,P_Bar_Function_Five(Table5.P_Bar),'r-') 
 
@@ -356,7 +363,7 @@ xlabel('Temperature [k]')
 ylabel('Height [km]')
 
 figure(5)
-%sgtitle('\phi = 85^{\circ}')
+sgtitle('\phi = 85^{\circ}')
 subplot(2,4,1)
 plot(Table6.P_Bar,Table6.Var1,'bo',Table6.P_Bar,P_Bar_Function_Six(Table6.P_Bar),'r-') 
 
@@ -408,7 +415,7 @@ ylabel('Height [km]')
 
 
 figure(6)
-%sgtitle('1972 - Venus Atmosphere (Model 1) (most probable molecular mass and mean solar activity)')
+sgtitle('1972 - Venus Atmosphere (Model 1) (most probable molecular mass and mean solar activity)')
 subplot(2,2,1)
 plot(Table7.Density*1000, Table7.Altitude,'bo',Density, Density_Function(Density),'r-')
 ylim([0 100])
@@ -452,8 +459,8 @@ title('Stable Altitudes - Mass and Volume','fontsize',18)
 legend('50 [km]','60 [km]','70 [km]','80 [km]','53 [km]','Vega 1 & 2','NASA Venus Reference (VDRM)','Cupid','Aeneas','location','Northwest')
 xlabel('Volume of Balloon [m^3]','fontsize',18)
 ylabel('Mass of Balloon [kg]','fontsize',18)
-ylim([0 900])
-xlim([0 1250])
+ylim([0 1250])
+xlim([0 800])
 
 %% Temperature and Pressure
 
@@ -508,5 +515,4 @@ xlim([0 1250])
 % H = 150;          %km
 % 
 % FSL = 20*log(d_km)+20*log(freq)+92.45;
-%% 
- 
+%%
